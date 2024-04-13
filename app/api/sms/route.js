7cacbed5cb5be3d3f3965ebb51a51d27220e72a1
Zoran Dasic <zorandsc@yahoo.com>
2024-04-13 15:06:12 +0200

    added twilio

---
+++ app/api/sms/route.js
@@ -0,0 +1,24 @@
+// Your AccountSID and Auth Token from console.twilio.com
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+export const POST = async (request, response) => {
+  console.log(request);
+  try {
+    const message = await client.messages.create({
+      body: "Svdbeni Cvet ima novu poruku za vas na zorand666@gmail.com",
+      to: "+38766234417", // Text your number
+      from: "+12514281722", // From a valid Twilio number
+    });
+    return new Response(
+      JSON.stringify({ success: true, messageSid: message }),
+      { status: 201 }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ success: false, error: error.message }),
+      { status: 500 }
+    );
+  }
+};
